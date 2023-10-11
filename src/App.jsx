cd1adc82da61dd4b422735b9a8000e3fba433cfe
rianfaisalm <rian.faisalm@gmail.com>
2023-10-11 13:45:16 +0700

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import { useRoutes } from "react-router";
+import UserDetail from "./pages/Userdetail";
+import ProtectedRoute from "./routes/Protectedroute";
+import Register from "./pages/Register";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const element = useRoutes(routes);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return element;
 }
 
 export default App
