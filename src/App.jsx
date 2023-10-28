3853ccf3257467bbf63bf86a69e0acb88934d7af
rianfaisalm <rian.faisalm@gmail.com>
2023-10-28 22:07:34 +0700

    Nambah protected route App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,31 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
-import Register from "./pages/Register"
 import { useRoutes } from "react-router";
 import "./App.css";
 import UserDetail from "./pages/Userdetail";
+import ProtectedRoute from "./routes/Protectedroute";
+import Register from "./pages/Register";
 
 const routes = [
-  { path: "/", element: <Homepage /> },
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Homepage />
+      </ProtectedRoute>
+    ),
+  },
   { path: "/login", element: <Login /> },
   { path: "/register", element: <Register /> },
-  { path: "/detail/:userId", element: <UserDetail /> },
+  {
+    path: "/detail/:userId",
+    element: (
+      <ProtectedRoute>
+        <UserDetail />
+      </ProtectedRoute>
+    ),
+  },
 ];
 
 function App() {
